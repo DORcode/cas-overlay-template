ca4018bc415afd065ee3a570bf61a509dc82950e
kh <dataanalyst@163.com>
2020-07-29 20:13:06 +0800

    spring.factories

---
+++ src/main/java/com/coin/config/CasConfig.java
@@ -0,0 +1,16 @@
+package com.coin.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName CasConfig
+ * @Description: TODO
+ * @Author kh
+ * @Date 2020-07-29 19:57
+ * @Version V1.0
+ **/
+@Configuration
+@ComponentScan("com.coin")
+public class CasConfig {
+}
---
+++ src/main/java/com/coin/web/TestController.java
@@ -0,0 +1,22 @@
+package com.coin.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @ClassName TestController
+ * @Description: TODO
+ * @Author kh
+ * @Date 2020-07-29 19:09
+ * @Version V1.0
+ **/
+@RestController
+public class TestController {
+    @GetMapping("/user")
+    public Object user(HttpServletRequest request) {
+        return request.getUserPrincipal();
+
+    }
+}

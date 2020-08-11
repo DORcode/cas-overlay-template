a184a8c83dccda9dfcb2e26058fe86c8442b5239
kh <dataanalyst@163.com>
2020-08-11 10:41:04 +0800

    swagger

--- src/main/java/com/coin/config/handler/UsernamePasswordCaptchaAuthenticationHandler.java
+++ src/main/java/com/coin/config/handler/UsernamePasswordCaptchaAuthenticationHandler.java
@@ -7,6 +7,7 @@ import org.apereo.cas.authentication.*;
 import org.apereo.cas.authentication.handler.support.AbstractPreAndPostProcessingAuthenticationHandler;
 import org.apereo.cas.authentication.principal.PrincipalFactory;
 import org.apereo.cas.authentication.principal.PrincipalResolver;
+import org.apereo.cas.services.JpaServiceRegistry;
 import org.apereo.cas.services.RegexRegisteredService;
 import org.apereo.cas.services.ServicesManager;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -75,7 +76,8 @@ public class UsernamePasswordCaptchaAuthenticationHandler extends AbstractPreAnd
 
         Map<String, Object> map = new HashMap<>();
         map.put("username", rmpc.getUsername());
-
+        map.put("id", 1);
+        map.put("mobile", "18620121232");
         List<MessageDescriptor> warning = new ArrayList<MessageDescriptor>();
         return createHandlerResult(credential, principalFactory.createPrincipal(username, map), warning);
     }
